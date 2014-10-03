d4d0149362eab7974b652b40d1257ac1197dbb5b
Islam Wazery <wazery@ubuntu.com>
2014-10-04 00:52:24 +0200

    Required necessary helpers source

--- lib/vider.rb
+++ lib/vider.rb
@@ -1,10 +1,14 @@
 require "vider/version"
+require 'vider/helpers'
 
 module Vider
   module Rails
     class Engine < ::Rails::Engine
       initializer :append_dependent_assets_path, group: :all do |app|
+        app.config.assets.paths += %w( javascripts )
+
         app.config.assets.precompile += %w( jquery.vide.js )
+        # app.config.assets.precompile += %w( vider.js )
       end
     end
   end
---
+++ lib/vider/helpers.rb
@@ -0,0 +1,25 @@
+module ViderVideoTag
+
+  # A view helper for creating a video background element
+  def vider_tag bg, options={}
+
+    volume        = options[:volume] || 1
+    playback_rate = options[:playback_rate] || 1
+    muted         = options[:muted] || true
+    looping       = options[:loop] || true
+    autoplay      = options[:autoplay] || true
+    position      = options[:position] || "50% 50%"
+    poster_type   = options[:poster_type] || "detect"
+
+    video_options = "volume: #{volume}, playback_rate: #{playback_rate}, muted: #{muted}, loop: #{looping}, autoplay: #{autoplay}, position: #{position}, poster_type: #{poster_type}" || 'loop: false, muted: false, position: 0% 0%'
+
+    content_tag :div, '', id: 'vider-element', style: 'width: 1000px; height: 500px;', data: { vide_bg: bg, vide_options: video_options }
+  end
+
+  # A view helper for creating adding a video background to an existing element
+  def vider_for_element_tag bg, options={}
+  end
+end
+
+# Add gem's view helpers to ActionView
+ActionView::Base.send :include, ViderVideoTag if defined? ActionView::Base
