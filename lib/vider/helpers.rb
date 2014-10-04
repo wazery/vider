6a11ff1c98986b591e63f440618d052b98edbd85
Islam Wazery <wazery@ubuntu.com>
2014-10-04 03:25:05 +0200

    Added width and height options

--- lib/vider/helpers.rb
+++ lib/vider/helpers.rb
@@ -1,8 +1,8 @@
 module ViderVideoTag
-
   # A view helper for creating a video background element
   def vider_tag bg, options={}
-
+    width         = options[:width]  || '1000px'
+    height        = options[:height] || '500px'
     volume        = options[:volume] || 1
     playback_rate = options[:playback_rate] || 1
     muted         = options[:muted] || true
@@ -13,10 +13,11 @@ module ViderVideoTag
 
     video_options = "volume: #{volume}, playback_rate: #{playback_rate}, muted: #{muted}, loop: #{looping}, autoplay: #{autoplay}, position: #{position}, poster_type: #{poster_type}" || 'loop: false, muted: false, position: 0% 0%'
 
-    content_tag :div, '', id: 'vider-element', style: 'width: 1000px; height: 500px;', data: { vide_bg: bg, vide_options: video_options }
+    content_tag :div, '', id: 'vider-element', style: "width: #{width}; height: #{height};", data: { vide_bg: bg, vide_options: video_options }
   end
 
   # A view helper for creating adding a video background to an existing element
+  # TODO: Implement this helper
   def vider_for_element_tag bg, options={}
   end
 end
